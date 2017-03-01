741436ac393972b65adb3a8ca8a159a105c0c4b2
bios <bios@spamers.net>
2017-03-01 06:47:32 +0100

    Added a Readme.md, cleanup

---
+++ README.md
@@ -0,0 +1,9 @@
+# Some scripts to automate tasks in NetworkManager
+
+## Install
+* Clone the repository
+* symlink or put the desired scripts into /etc/NetworkManager/dispatcher.d.
+* Add execution rights to the scripts with `chmod +x SCRIPTNAME`
+
+
+
--- wlan_auto_toggle.sh
+++ wlan_auto_toggle.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
 # network manager dispatcher script to disable
-#  wifi when in Dock.
+#  wifi when in Dock and the cable ethernet interface comes up.
 #  Installation:
 #     * put in /etc/NetworkManager/dispatch.d/
-#     * chmod +x he script
+#     * chmod +x the script
+
+
+# afterwards wlan can be manually enabled again and will stay up until 
+#  the next change to the interface
 
 # Original source: http://superuser.com/questions/233448/disable-wlan-if-wired-cable-network-is-available
+# TODO add (auto)config for different interfaces
+
 if [ "$1" = "eth0" ]; then
     case "$2" in
         up)
